c75ee60c995bd2e05e0decc0d155562c63ddd5e1
hustart <hustart1@126.com>
2023-04-16 22:09:06 +0800

    update: 添加junit

--- src/test/java/com/yago/netty/test/BaseTest.java
+++ src/test/java/com/yago/netty/test/BaseTest.java
@@ -1,6 +1,8 @@
 package com.yago.netty.test;
 
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @author hustart1@126.com
@@ -8,5 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
  * @createTime 2023年04月16日 21:11:00
  */
 @SpringBootTest
+@RunWith(SpringRunner.class)
 public class BaseTest {
+
 }
