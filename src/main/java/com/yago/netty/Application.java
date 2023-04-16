e54676f3e0c82de134d641d8b9d1a8660e998946
hustart <hustart1@126.com>
2023-04-16 21:55:02 +0800

    init: 项目初始化

---
+++ src/main/java/com/yago/netty/Application.java
@@ -0,0 +1,17 @@
+package com.yago.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hustart1@126.com
+ * @Description
+ * @createTime 2023年04月16日 21:01:00
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args){
+	    SpringApplication.run(Application.class,args);
+	}
+
+}
---
+++ src/test/java/com/yago/netty/test/BaseTest.java
@@ -0,0 +1,12 @@
+package com.yago.netty.test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author hustart1@126.com
+ * @Description
+ * @createTime 2023年04月16日 21:11:00
+ */
+@SpringBootTest
+public class BaseTest {
+}
